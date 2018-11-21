c5ee5122eeb310bf046c1d2f2fc9dcb43632e833
julenb <21701719@live.uem.es>
2018-11-21 16:42:58 +0100

    import StageStyle

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
