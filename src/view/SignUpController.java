54c665788b7d9a54c11b071c7b4a2d2283bd7c8c
julenb <21701719@live.uem.es>
2018-11-27 16:14:48 +0100

    Función de cerrar ventana

--- src/view/SignUpController.java
+++ src/view/SignUpController.java
@@ -1,7 +1,16 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 public class SignUpController {
 
-    
+    @FXML
+    private ImageView imgExit;
+
+    public void exit() {
+        ((Stage) imgExit.getScene().getWindow()).close();
+    }
 
 }
